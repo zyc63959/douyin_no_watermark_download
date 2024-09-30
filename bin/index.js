fc4759f22f1d8e494504c578550b15e40194f2b2
zyc63959 <51981348+zyc63959@users.noreply.github.com>
2024-09-30 17:26:54 +0800

    add base

--- bin/index.js
+++ bin/index.js
@@ -151,6 +151,37 @@ class Scraper {
         }
         return noWatermarkUrls;
     }
+     /**
+     * @description parser no watermark video and image url
+     * @param {object} videoData 
+     * @returns {object}
+     */
+async getBaseInfo(videoData) {
+    let noWatermarkUrl;
+    const nickname = videoData.aweme_detail.author.nickname;
+    const desc = videoData.aweme_detail.desc;
+    const fileName = `${nickname} - ${desc} 【抖音】`;
+    const idm = {}
+    const isFyp = videoData.aweme_detail.media_type === 2
+    if (!isFyp) {
+        let url = videoData.aweme_detail.video.play_addr.url_list[0];
+        let key = videoData.aweme_detail.video.play_addr.uri.replace('video/', '');
+        noWatermarkUrl = url.replace('/play/', '/playwm/') + '?video_id=' + key;
+        idm.url = noWatermarkUrl
+        idm.name = fileName
+    } else {
+        // 图片分享
+        let images = videoData?.aweme_detail?.images
+        noWatermarkUrl = images.map(i => {
+            if (!i?.url_list) return null
+            const maxSizePicIndex = i?.url_list.length - 1
+            return i?.url_list[maxSizePicIndex]
+        }).filter(i => i)
+        idm.url = noWatermarkUrl.toString()
+        idm.name = Array.from({ length: noWatermarkUrl.length }, (_, index) => `${nickname} - ${desc}${index}【抖音】`).join();
+    }
+    return { fileName, noWatermarkUrl, idm };
+}
 
     /**
      * @description parser watermark video url
